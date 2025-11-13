56c0ad460075494380a4b0ccc34f585d096e44bc
freshmanacadamy <freshmanacadamy@gmail.com>
2025-11-13 16:53:23 +0300

    Create Config.js

---
+++ Config.js
@@ -0,0 +1,18 @@
+{
+  "name": "ju-marketplace-bot",
+  "version": "1.0.0",
+  "description": "Jimma University Marketplace Telegram Bot",
+  "scripts": {
+    "start": "node api/bot.js",
+    "dev": "node api/bot.js"
+  },
+  "dependencies": {
+    "node-telegram-bot-api": "^0.64.0",
+    "express": "^4.18.2",
+    "firebase-admin": "^11.11.1",
+    "dotenv": "^16.3.1"
+  },
+  "engines": {
+    "node": "18.x"
+  }
+}
